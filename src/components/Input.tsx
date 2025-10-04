8e91aab46c5a5b4ef1afe7bb3ef11772fdebc451
Lucas Camilo <lcscamilo1998@gmail.com>
2025-10-04 08:38:51 -0300

    chore: initial commit (vite install, initial components, AuthLayout, auth-routes and Input)

---
+++ src/components/Input.tsx
@@ -0,0 +1,21 @@
+type Props = React.ComponentProps<"input"> & {
+  legend?: string;
+};
+
+export function Input({ legend, ...rest }: Props) {
+  return (
+    <fieldset className="flex flex-1 max-h-20 text-gray-200 focus-within:text-green-100">
+      {legend && (
+        <legend className="uppercase text-xxs  mb-2 text-inherit">
+          {legend}
+        </legend>
+      )}
+
+      <input
+        {...rest}
+        type="text"
+        className="w-full h-12 rounded-lg border border-gray-300 px-4 text-sm text-gray-100 bg-transparent focus:border-green-100 focus:ring-0 focus:outline-none focus:border-1 placeholder-gray-200"
+      />
+    </fieldset>
+  );
+}
